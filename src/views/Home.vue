1cea69bc32b299687fe91e7e3f3e955ea3366efc
saulmamani <luas0_1@yahoo,es>
2021-05-07 12:26:30 -0400

    paginacion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,6 +32,7 @@
               color="primary"
               depressed
               block
+              @click="pagination.current_page = 1; getProductos()"
           >
             <v-icon>mdi-magnify</v-icon>
             Buscar
@@ -40,6 +41,17 @@
       </v-row>
 
       <v-row>
+        <v-col cols="12" md="12" sm="12">
+            <v-pagination
+                v-model="pagination.current_page"
+                :length="pagination.last_page"
+                @input="getProductos"
+                :total-visible="7"
+                circle
+                small
+            />
+        </v-col>
+        
         <v-col cols="12" md="4" sm="6" v-for="(item, index) in productos" :key="index">
           <producto
             :producto="item"
@@ -62,7 +74,13 @@ export default {
   components: {Producto},
   data: () => ({
     productos: [],
-    loading: false
+    loading: false,
+    //paginacion
+    pagination: {
+      per_page: 0,
+      current_page: 1,
+      last_page: 0,
+    },
   }),
   computed: {
     ...mapState(['url']),
@@ -79,14 +97,20 @@ export default {
     },
     getProductos() {
       this.loading = true
-      const url = this.url + "productos";
+      const url = this.url + "productos?page=" + this.pagination.current_page;
       this.axios.get(url).then(response => {
-        this.productos = response.data
+        this.productos = response.data.data
+        this.makePagination(response.data);
       }).catch(e => {
         this.$toastr.error(e.error.data)
       }).finally(() => {
         this.loading = false;
       });
+    },
+    makePagination: function (page) {
+      this.pagination.per_page = page.per_page;
+      this.pagination.current_page = page.current_page;
+      this.pagination.last_page = page.last_page;
     }
   }
 }
